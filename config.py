06c285f8b7bcb42f06223781e7e133deaaf3d49a
abbracadabra <2062460209@qq.com>
2019-04-29 15:32:33 +0800

    v1

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 basedir = os.getcwd()
 log_path = os.path.join(basedir,'log')
 epochs = 30
-anchors=[[0.57273/13, 0.677385/13], [1.87446/13, 2.06253/13], [3.33843/13, 5.47434/13], [7.88282/13, 3.52778/13], [9.77052/13, 9.16828/13]]
 model_path = os.path.join(basedir,'mdl','mdl')
 vocimdir = r"D:\Users\yl_gong\Desktop\dl\voc\VOC2012\JPEGImages"
 voclabeldir= r"D:\Users\yl_gong\Desktop\dl\voc\VOC2012\Annotations"
