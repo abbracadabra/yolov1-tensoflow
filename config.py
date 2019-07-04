c15d85967ab3061f160485100cab76afcad9970b
Steven Lee <gyl_mail@qq.com>
2019-07-04 15:57:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ basedir = os.getcwd()
 log_path = os.path.join(basedir,'log')
 epochs = 10
 model_path = os.path.join(basedir,'mdl','mdl')
-vocimdir = r"D:\Users\yl_gong\Desktop\dl\voc\combo\image"
-voclabeldir= r"D:\Users\yl_gong\Desktop\dl\voc\combo\label"
+vocimdir = r"D:\Users\xxx\Desktop\dl\voc\combo\image"
+voclabeldir= r"D:\Users\xxx\Desktop\dl\voc\combo\label"
 trainbatch=10
 labels=['aeroplane','bicycle','bird','boat','bottle','bus','car','cat','chair','cow','diningtable','dog',
                         'horse','motorbike','person','pottedplant','sheep','sofa','train','tvmonitor']
